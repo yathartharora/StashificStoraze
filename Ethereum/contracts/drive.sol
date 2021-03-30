3935e82882ce0b617ab5e980ad36ec5ca43a100d
Yatharth <yathartharora1999@gmail.com>
2021-03-30 13:30:33 +0530

    Display updated

--- Ethereum/contracts/drive.sol
+++ Ethereum/contracts/drive.sol
@@ -7,16 +7,19 @@ contract Drive{
     struct File{
         string link;
         string name;
+        string date;
+        string format;
     }
     
 
     mapping(address => bool) public isRegistered;
+
     mapping(address => bytes4) public registered;
     mapping(address => File[]) public file;
     
-    function upload(string memory file_name, string memory file_link) public{
+    function upload(string memory file_name, string memory file_link, string memory d, string memory f) public{
         require(isRegistered[msg.sender]);
-        file[msg.sender].push(File(file_name,file_link));
+        file[msg.sender].push(File(file_name,file_link,d,f));
     }
     
     function registerTodrive(bytes4 pass) public{
--- Ethereum/drive.js
+++ Ethereum/drive.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import Drive from './build/Drive.json';
 
-const instance = new web3.eth.Contract((Drive.abi),'0x433FE4171ec88Dcb0bb3Fc512139edFbD8E1237a');
+const instance = new web3.eth.Contract((Drive.abi),'0xcD1e0b3EDa74EE2eEB67b85D393a1a27578A5837');
 
 
 export default instance;
